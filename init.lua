6c6d36b76eea4452b08876ea17e69f2290118d89
Vladislav Isaychev <gregnov69@gmail.com>
2025-08-29 01:51:04 +0300

    deleted comments

--- init.lua
+++ init.lua
@@ -2,8 +2,6 @@ require 'config.options'
 require 'config.keymap'
 require 'config.autocommands'
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -17,31 +15,7 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `opts` key (recommended), the configuration runs
-  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
   require 'plugins.which_key',
   require 'plugins.telescope',
   require 'plugins.lazydev_config',
@@ -55,6 +29,7 @@ require('lazy').setup({
   require 'plugins.breadcrumbs',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+  --
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
